1526baa1e68524702087c82c700d07352eec224a
Newton <65068273+newtonHesienberg@users.noreply.github.com>
2022-04-09 15:31:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 from flask import Flask, render_template, request
 import os
 
+from Katna.video import Video
+from Katna.writer import KeyFrameDiskWriter
+
 app = Flask(__name__)
 
-@app.route('/', methods = ["GET", "POST"])
+no_of_frames_to_return = 10
+diskwriter = KeyFrameDiskWriter(location="KeyFrames")
+
+
+@app.route('/', methods=["GET", "POST"])
 def Hello():
     if request.method == 'POST':
         file = request.files["file"]
         file.save(os.path.join("Video_Uploads", file.filename))
-        return render_template("base.html", message = "Success")
-    return render_template("base.html", message = "Upload") 
+        vd.extract_video_keyframes(
+            no_of_frames=no_of_frames_to_return, file_path= "Video_Uploads/" + file.filename,
+            writer=diskwriter
+        )
+        
+        return render_template("base.html", message="Success")
+    return render_template("base.html", message="Upload")
+
 
 if __name__ == "__main__":
-    app.run("localhost", 1000, debug=True)    
+    vd = Video()
+    app.run("localhost", 1000, debug=True)
