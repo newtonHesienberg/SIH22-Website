408d4e3ceb1136602dd663fde2a39d0bb4fca33d
Newton <65068273+newtonHesienberg@users.noreply.github.com>
2022-04-09 21:38:35 +0530

    Video Process Without Reload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from werkzeug.utils import redirect
 import os
 
 from Katna.video import Video
@@ -7,23 +8,31 @@ from Katna.writer import KeyFrameDiskWriter
 app = Flask(__name__)
 
 no_of_frames_to_return = 10
-diskwriter = KeyFrameDiskWriter(location="KeyFrames")
+global fileName
+fileName = ''
 
 
 @app.route('/', methods=["GET", "POST"])
 def Hello():
     if request.method == 'POST':
+        global fileName
         file = request.files["file"]
+        fileName = file.filename
         file.save(os.path.join("Video_Uploads", file.filename))
-        vd.extract_video_keyframes(
-            no_of_frames=no_of_frames_to_return, file_path= "Video_Uploads/" + file.filename,
-            writer=diskwriter
-        )
-        
-        return render_template("base.html", message="Success")
-    return render_template("base.html", message="Upload")
+        return redirect('/')
+        return render_template("base.html", message="Video Successfully Uploaded...")
+    return render_template("base.html", message="Upload a Video...")
 
+
+@app.route('/keyframe')
+def keyframe():
+    global fileName
+    vd = Video()
+    vd.extract_video_keyframes(
+        no_of_frames=no_of_frames_to_return, file_path="Video_Uploads/" + fileName,
+        writer = KeyFrameDiskWriter(location="KeyFrames")
+    )
+    # Show KeyFrames Extracted Successfully... message
 
 if __name__ == "__main__":
-    vd = Video()
     app.run("localhost", 1000, debug=True)
