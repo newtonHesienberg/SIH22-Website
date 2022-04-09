db53e1ae5394de2231e231128dec4a6ba23457c1
Newton <65068273+newtonHesienberg@users.noreply.github.com>
2022-04-10 00:15:57 +0530

    Keyframe extracted successfully...

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from werkzeug.utils import redirect
 import os
 
@@ -19,20 +19,28 @@ def Hello():
         file = request.files["file"]
         fileName = file.filename
         file.save(os.path.join("Video_Uploads", file.filename))
-        return redirect('/')
+        redirect('/')
         return render_template("base.html", message="Video Successfully Uploaded...")
     return render_template("base.html", message="Upload a Video...")
 
 
-@app.route('/keyframe')
-def keyframe():
+def extractKeyFrame():
     global fileName
     vd = Video()
     vd.extract_video_keyframes(
         no_of_frames=no_of_frames_to_return, file_path="Video_Uploads/" + fileName,
-        writer = KeyFrameDiskWriter(location="KeyFrames")
+        writer=KeyFrameDiskWriter(location="KeyFrames")
     )
+    print("KeyFrames Extracted Successfully...")
+
+
+@app.route('/keyframe')
+def keyframe():
+    extractKeyFrame()
+    results = {'processed': 'True'}
+    return jsonify(results)
     # Show KeyFrames Extracted Successfully... message
+
 
 if __name__ == "__main__":
     app.run("localhost", 1000, debug=True)
